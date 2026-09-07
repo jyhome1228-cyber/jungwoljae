972adfba372e971e19fc0b6580afa411ea6851f4
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-09-07 16:19:48 +0900

    Bump footer and auth asset versions

--- app.js
+++ app.js
@@ -174,5 +174,5 @@ if(document.querySelector('#reviews') && document.body.dataset.reviewsPage!=='tr
 }
 
 const footerScript=document.createElement('script');
-footerScript.src='./footer.js?v=20260907-1548';
+footerScript.src='./footer.js?v=20260907-02';
 document.body.appendChild(footerScript);
